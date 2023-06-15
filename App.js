3b46fa0dd7cf4b4d869b15376e175d88353fa0d0
Abhishekbatraab <abhishekbatra001@gmail.com>
2023-06-15 20:21:32 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,28 +1,20 @@
-/** 
- * <div id="parent">
- *      <div id="child">
- *          <h1>I'm a h1 tag</h1>
- *          <h2>I am a h2 tag</h2>
- *      </div>
- * </div>
- * 
- * ReactElement(Object) => HTML(Browser Understands)
- * 
- * */
+import React from "react";
+import ReactDOM from 'react-dom/client';
 
 const parent = React.createElement("div", {id: 'parent'}, 
     [
         React.createElement("div", {id:"child"}, 
-            [React.createElement("h1", {}, "I'm a h1 tag"), 
-            React.createElement("h2", {}, "I'm a h2 tag")]
+            [React.createElement("h1", {}, "This is Namaste React 🚀"), 
+            React.createElement("h2", {}, "by Abhishek Batra")]
         ),
         React.createElement("div", 
         {id:"child"}, [React.createElement("h1", {}, "I'm a h1 tag"), 
         React.createElement("h2", {}, "I'm a h2 tag")])
     ]
 );
+console.log(parent); //object
 
-//JSX -> Why JSX is introduced => to make liffe easier writing code or react.
+//JSX -> Why JSX is introduced => to make life easier writing code or react.
  
 
 
